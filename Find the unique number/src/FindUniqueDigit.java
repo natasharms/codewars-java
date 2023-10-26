772fa29696a4e18cdc4fe1c3fc82d6b8b7116f0d
Natasha Ramos <natasharmsc@gmail.com>
2023-10-26 20:08:36 -0300

    find unique digit

---
+++ Find the unique number/src/FindUniqueDigit.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class FindUniqueDigit {
+    public static double findUniq(double arr[]) {
+        Arrays.sort(arr);
+        return (arr[0] != arr[1]) ? arr[0] : arr[arr.length - 1];
+    }
+}
---
+++ Find the unique number/src/FindUniqueDigitTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+
+public class FindUniqueDigitTest {
+    private double precision = 0.0000000000001;
+
+    @Test
+    public void sampleTestCases() {
+        assertEquals(1.0, FindUniqueDigit.findUniq(new double[]{0, 1, 0}), precision);
+        assertEquals(2.0, FindUniqueDigit.findUniq(new double[]{1, 1, 1, 2, 1, 1}), precision);
+    }
+}
